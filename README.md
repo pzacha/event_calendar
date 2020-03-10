8c08c262a565f12f1b66818cfc4e88f7145d367e
Piotr Zacha <pzacha11@gmail.com>
2020-03-10 11:44:26 +0100

    Created Event model

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 * python 3.8.0
 	* Django 3.0.4
 
+## TODO
+List of features ready and TODOs for future development
+* User model (many-to-many relationship with Event model)
+* User authentication
+* 
+
 ## Status
 Project is: _in_progress_
 
--- event_calendar/events/models.py
+++ event_calendar/events/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    desc = models.TextField("Event description", blank=True, max_length=1500)
+
+    def __str__(self):
+        return self.name
+
+    def status(self):
+        if self.start_date > timezone.now():
+            return "Upcoming"
+        elif self.start_date <= timezone.now() and self.end_date >= timezone.now():
+            return "Ongoing"
+        else:
+            return "Ended"
+
