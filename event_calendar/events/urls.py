e103d54d345cb19e90d0a2f3e7f0d808733e0f44
Piotr Zacha <pzacha11@gmail.com>
2020-03-10 19:12:08 +0100

    Added views and templates

--- event_calendar/events/urls.py
+++ event_calendar/events/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
 from . import views
+
+app_name = "events"
+urlpatterns = [
+    path("", views.EventsView.as_view(), name="Events"),
+    path("<int:pk>/", views.DetailView.as_view(), name="Detail"),
+]
--- event_calendar/events/views.py
+++ event_calendar/events/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Event
+
+
+class EventsView(generic.ListView):
+    template_name = "events/events.html"
+    context_object_name = "all_events_list"
+
+    def get_queryset(self):
+        """Return all upcoming or ongoing events"""
+        return Event.objects.order_by("start_date")
+
+    # Do not show past events
+
+
+class DetailView(generic.DetailView):
+    model = Event
+    template_name = "events/detail.html"
+
+
+# ongoing events
+# show closest events
+# show past events
