3d682fdc9a6602b9f6447af86f0546514cb50190
Piotr Zacha <pzacha11@gmail.com>
2020-03-18 15:51:56 +0100

    Log out functionality added.

--- event_calendar/users/views.py
+++ event_calendar/users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Created {username} account!")
-            return redirect("events:Calendar")
+            return redirect("events:Login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
