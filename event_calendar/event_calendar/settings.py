29cb1ef812b8a59074639236b9697b3fc46ddb87
Piotr Zacha <pzacha11@gmail.com>
2020-03-18 15:31:01 +0100

    Login template added.

--- event_calendar/event_calendar/settings.py
+++ event_calendar/event_calendar/settings.py
@@ -119,3 +119,5 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "events:Calendar"
--- event_calendar/users/urls.py
+++ event_calendar/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,14 @@ app_name = "users"
 
 urlpatterns = [
     path("register/", views.register, name="Register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="Login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="Logout",
+    ),
 ]
