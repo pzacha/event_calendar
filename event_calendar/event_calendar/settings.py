32853b71de3604b4eb8ff05feb322e239d1af2db
Piotr Zacha <pzacha11@gmail.com>
2020-03-23 18:22:04 +0100

    Reverting to commit 1c31a3e

--- event_calendar/event_calendar/settings.py
+++ event_calendar/event_calendar/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "5791628bb0b13ce0c676dfde280ba245"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == "True"
@@ -131,6 +129,3 @@ LOGIN_REDIRECT_URL = "events:Calendar"
 LOGIN_URL = "users:Login"
 
 AUTH_USER_MODEL = "users.CustomUser"
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
