f93768d2a3c70b271c8217e21772ae9c6060ca5a
Piotr Zacha <pzacha11@gmail.com>
2020-03-24 15:21:33 +0100

    Updated settings and requirements.txt

--- event_calendar/event_calendar/settings.py
+++ event_calendar/event_calendar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ LOGIN_REDIRECT_URL = "events:Calendar"
 LOGIN_URL = "users:Login"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+django_heroku.settings(locals())
