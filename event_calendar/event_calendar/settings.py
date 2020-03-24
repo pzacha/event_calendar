af714ddb2b177e6a7d2c7923136b81fad54963db
Piotr Zacha <pzacha11@gmail.com>
2020-03-24 12:52:38 +0100

    Settings SECRET_KEY, DEBUG and ALLOWED_HOSTS values update

--- event_calendar/event_calendar/settings.py
+++ event_calendar/event_calendar/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "5791628bb0b13ce0c676dfde280ba245"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "evcal.herokuapp.com"]
 
 
 # Application definition
