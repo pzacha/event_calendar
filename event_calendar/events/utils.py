716483fd597dad4c9cd1c53ee4b93328defca31e
Piotr Zacha <pzacha11@gmail.com>
2020-03-25 09:56:48 +0100

    Bug fix in Calendar class.

--- event_calendar/events/utils.py
+++ event_calendar/events/utils.py
@@ -11,13 +11,15 @@ class Calendar(HTMLCalendar):
         super().__init__()
 
     def formatday(self, day, events):
-        events_per_day = events.filter(start_date__day__lte=day).filter(
-            end_date__day__gte=day
-        )
-        event_html = ""
-        for event in events_per_day:
-            event_html += event.get_event_url() + "<br>"
         if day != 0:
+            current_day = datetime(self.year, self.month, day)
+            events_per_day = events.filter(
+                start_date__lte=(current_day + timedelta(days=1))
+            ).filter(end_date__gte=current_day)
+            event_html = ""
+            for event in events_per_day:
+                event_html += event.get_event_url() + "<br>"
+
             return f"<td><span class='date'>{day}</span><ul> {event_html} </ul></td>"
         return "<td></td>"
 
@@ -26,8 +28,10 @@ class Calendar(HTMLCalendar):
         return f"<tr> {week_html} </tr>"
 
     def formatmonth(self):
-        events = Event.objects.filter(
-            start_date__year=self.year, start_date__month=self.month
+        events = (
+            Event.objects.filter(start_date__year=self.year)
+            .filter(start_date__month__lte=self.month)
+            .filter(end_date__month__gte=self.month)
         )
         cal = f'<table border="4" cellpadding="20" cellspacing="0" class="calendar">\n'
         cal += f"{self.formatmonthname(self.year, self.month)}\n"
